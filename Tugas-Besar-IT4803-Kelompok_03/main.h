07c58767f2fe0168d30add4bc3a96fd368bef946
Windy Dwi Febrianti <windydf25@gmail.com>
2025-11-27 23:16:29 +0700

    Update main.h

---
+++ Tugas-Besar-IT4803-Kelompok_03/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <iostream>
+
+using namespace std;
+
+void menuAdmin();
+void menuDokter();
+void menuPasien();
+void menuUser();
+#endif // MAIN_H_INCLUDED
