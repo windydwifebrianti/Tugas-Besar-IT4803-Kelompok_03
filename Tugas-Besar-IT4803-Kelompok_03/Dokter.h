6984a3e930fd0d43159ebf2907c98508ba6c0409
ares <arkeiiuc3@gmail.com>
2025-12-02 16:29:12 +0700

    Update Dokter.h with new parameter
    Added new functions for inserting and deleting doctors in the list.

--- Tugas-Besar-IT4803-Kelompok_03/Dokter.h
+++ Tugas-Besar-IT4803-Kelompok_03/Dokter.h
@@ -28,13 +28,15 @@ adr_Dokter newDokter(Dokter data);
 
 void insert_first_Dokter(list_Dokter &L, adr_Dokter P);
 void insert_last_Dokter(list_Dokter &L, adr_Dokter P);
-void insert_after_Dokter(list_Dokter &L, adr_Dokter P);
+void insert_after_Dokter(list_Dokter &L, adr_Dokter P, adr_Dokter Prec);
 
-void delete_first_Dokter(list_Dokter &dokterList, list_Relasi &relasiList, string idDokter);
-void delete_last_Dokter(list_Dokter &dokterList, list_Relasi &relasiList, string idDokter);
-void delete_after_Dokter(list_Dokter &dokterList, list_Relasi &relasiList, string idDokter);
+void delete_first_Dokter(list_Dokter &DL, list_Relasi &RL, string idDokter);
+void delete_last_Dokter(list_Dokter &DL, list_Relasi &RL, string idDokter);
+void delete_after_Dokter(list_Dokter &DL, list_Relasi &RL, adr_Dokter Prec, string idDokter);
 
 adr_Dokter findDokter(list_Dokter L, string idDokter);
 void showAllDokter(list_Dokter L);
+
+void deleteRelasiByDokter(list_Relasi &relasiList, string idDokter);
 
 #endif // DOKTER_H_INCLUDED
