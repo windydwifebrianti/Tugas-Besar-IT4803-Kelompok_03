edd6ff8b8c6589acfafe44000e504f06f8655805
ares <arkeiiuc3@gmail.com>
2025-12-14 15:38:20 +0700

    Refactor Relation.h with new structure and functions

--- Tugas-Besar-IT4803-Kelompok_03/Relation.h
+++ Tugas-Besar-IT4803-Kelompok_03/Relation.h
@@ -2,42 +2,41 @@
 #define RELATION_H_INCLUDED
 
 #include <iostream>
-
+#include "Dokter.h"
+#include "Pasien.h"
 using namespace std;
-
-struct Dokter;
-typedef struct elm_Dokter *adr_Dokter;
-struct Pasien;
-typedef struct elm_Pasien *adr_Pasien;
 
 typedef struct elm_Relasi *adr_Relasi;
 
-struct elm_Relasi
-{
+struct elm_Relasi{
     adr_Dokter nextD;
     adr_Pasien nextP;
     adr_Relasi next;
-    adr_Relasi prev; 
 };
 
-struct list_Relasi
-{
+struct list_Relasi{
     adr_Relasi first;
-    adr_Relasi last;
 };
 
 void createListRelasi(list_Relasi &L);
 adr_Relasi newRelasi(adr_Dokter Dokter, adr_Pasien Pasien);
+
+void insertRelasi(list_Relasi &L, adr_Relasi p);
+
+void deleteFirstRelasi(list_Relasi &L, adr_Relasi &p);
+void deleteAfterRelasi(list_Relasi &L, adr_Relasi prec, adr_Relasi &p);
+void deleteLastRelasi(list_Relasi &L, adr_Relasi &p);
+void deleteRelasi(list_Relasi &L, adr_Relasi p);
 
 void ShowPasien_from_Dokter(list_Relasi L, string ID_Dokter);
 void ShowDokter_from_Pasien(list_Relasi L, string ID_Pasien);
 void ShowRelasiDokter_to_Pasien(list_Relasi L);
 void ShowRelasiPasien_to_Dokter(list_Relasi L);
 
-void insertRelasi(list_Relasi &L, adr_Dokter Dokter, adr_Pasien Pasien);
-void deleteRelasi(list_Relasi &L, string ID_Dokter, string ID_Pasien);
-adr_Relasi findRelasi(list_Relasi L, string ID_Dokter, string ID_Pasien);
-void editRelasi(list_Relasi &L, string ID_Pasien, string new_ID_Dokter);
+adr_Relasi findElmRelasiByChild(list_Relasi L, adr_Pasien Pasien);
+adr_Relasi findElmRelasiByChild(list_Relasi L, adr_Dokter Dokter);
+
+void editRelasiDokter(list_Relasi &L, string ID_Pasien, adr_Dokter dokterBaru);
 
 int jumPasien_to_Dokter(list_Relasi L, string ID_Dokter);
 int jumDokter_HasPasien(list_Relasi L);
