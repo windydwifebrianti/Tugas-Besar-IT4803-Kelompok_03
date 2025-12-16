c49fcbdd41c8f70b8854de9cd237abe37cd25325
Windy Dwi Febrianti <windydf25@gmail.com>
2025-12-16 19:24:55 +0700

    Refactor dokter functions to use new structure

--- Tugas-Besar-IT4803-Kelompok_03/dokter_103032430035.cpp
+++ Tugas-Besar-IT4803-Kelompok_03/dokter_103032430035.cpp
@@ -1,51 +1,54 @@
 #include "Dokter.h"
+#include <iostream>
+using namespace std;
 
 adr_Dokter newDokter(infotype_Dokter x) {
 	adr_Dokter P;
 	P = new elm_Dokter;
-	P -> info = x;
-	P -> next = nullptr;
+	P -> infoD = x;
+	P -> nextD = nullptr;
 	return P;
 }
 
 void insert_first_Dokter(list_Dokter &L, adr_Dokter P) {
-	if(L.first == nullptr) {        // jika daftar kosong
-		L.first = P;               // node baru menjadi node pertama
+	if(L.firstD == nullptr) {        // jika daftar kosong
+		L.firstD = P;               // node baru menjadi node pertama
 	} else {
-		P->next = L.first;         // hubungkan node baru ke node lama paling depan
-		L.first = P;               // jadikan node baru sebagai node pertama
+		P->nextD = L.firstD;         // hubungkan node baru ke node lama paling depan
+		L.firstD = P;               // jadikan node baru sebagai node pertama
 	}
 }
 
 
 adr_Dokter findDokter(list_Dokter &L, string idDokter, string spesialis) {
 	adr_Dokter P;
-	P = L.first;         						// mulai di node pertama
+	P = L.firstD;         						// mulai di node pertama
 	while(P != nullptr) {           	 		// teruskan selama ada node
-		if (P->info.ID_Dokter == idDokter && P->info.spesialis == spesialis) { 	// jika ID cocok
+		if (P->infoD.ID_Dokter == idDokter && P->infoD.spesialis == spesialis) { 	// jika ID cocok
 			return P;                 			// kembalikan node tersebut
 		}
-		P = P->next;                			// lanjut ke node berikutnya
+		P = P->nextD;                			// lanjut ke node berikutnya
 	}
 	return nullptr;                    // tidak ditemukan    
 }
 
 void delete_first_Dokter(list_Dokter &L, adr_Dokter &P){
-	if (L.first == nullptr) {
+	if (L.firstD == nullptr) {
 		P = nullptr; // List kosong, tidak ada yang dihapus
 	} else {
-		P = L.first;
-		L.first = P -> next;
-		P -> next = nullptr;
+		P = L.firstD;
+		L.firstD = P -> nextD;
+		P -> nextD = nullptr;
 	}
 }
 
 void delete_after_Dokter(list_Dokter &L, adr_Dokter Prec, adr_Dokter &P){
-	if (L.first == nullptr || Prec == nullptr || Prec->next == nullptr) {
+	if (L.firstD == nullptr || Prec == nullptr || Prec->nextD == nullptr) {
 		P = nullptr; // Tidak ada node setelah Prec atau list kosong
 	} else {
-		P = Prec -> next;	
-		Prec -> next = P -> next;
-		P -> next = nullptr;
+		P = Prec -> nextD;	
+		Prec -> nextD = P -> nextD;
+		P -> nextD = nullptr;
 }
+
 
