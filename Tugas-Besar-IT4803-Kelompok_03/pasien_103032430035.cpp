d03dacbc100a0686eccf4e0b8d2443655734c159
Windy Dwi Febrianti <windydf25@gmail.com>
2025-12-16 19:29:07 +0700

    Refactor list_Pasien structure and related functions

--- Tugas-Besar-IT4803-Kelompok_03/pasien_103032430035.cpp
+++ Tugas-Besar-IT4803-Kelompok_03/pasien_103032430035.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 void createListPasien(list_Pasien &L) {
-    L.first = nullptr;
-    L.last = nullptr;
+    L.firstP = nullptr;
+    L.lastP = nullptr;
 }
 
 void insert_last_Pasien(list_Pasien &L, adr_Pasien P) {
-  if (L.first == nullptr && L.last == nullptr) {
-    L.first = P;
-    L.last = P;
+  if (L.firstP == nullptr && L.lastP == nullptr) {
+    L.firstP = P;
+    L.lastP = P;
   } else {
-    L.last -> next = P;
-    P -> prev = L.last;
-    L.last = P;
+    L.lastP -> nextP = P;
+    P -> prevP = L.lastP;
+    L.lastP = P;
   }
 }
 
@@ -25,42 +25,43 @@ void insert_after_Pasien(list_Pasien &L, adr_Pasien Prec, adr_Pasien P) {
     return;
   }
 
-  if (Prec == L.last) {
-    P -> next = nullptr;
-    P -> prev = Prec;
-    Prec -> next = P;
-    L.last = P;
+  if (Prec == L.lastP) {
+    P -> nextP = nullptr;
+    P -> prevP = Prec;
+    Prec -> nextP = P;
+    L.lastP = P;
   } else {
-    P -> next = Prec -> next;
-    P -> prev = Prec;
-    Prec -> next = P;
-    P -> next -> prev = P;
+    P -> nextP = Prec -> nextP;
+    P -> prevP = Prec;
+    Prec -> nextP = P;
+    P -> nextP -> prevP = P;
   }
 }
 
 void delete_last_Pasien(list_Pasien &L, adr_Pasien &P){
-    if (L.first == nullptr && L.last == nullptr) {
+    if (L.firstP == nullptr && L.lastP == nullptr) {
       P = nullptr; // List kosong, tidak ada yang dihapus
       cout << "List kosong" << endl;
-    } else if (L.first == L.last) {
-        P = L.first;
-        L.first = nullptr;
-        L.last = nullptr;
+    } else if (L.firstP == L.lastP) {
+        P = L.firstP;
+        L.firstP = nullptr;
+        L.lastP = nullptr;
     } else {
-        P = L.last;
-        L.last = P -> prev;
-        L.last -> next = nullptr;
-        P -> prev = nullptr;
+        P = L.lastP;
+        L.lastP = P -> prevP;
+        L.lastP -> nextP = nullptr;
+        P -> prevP = nullptr;
     }
 }
 
 void showAllPasien(list_Pasien L){
-    adr_Pasien P = L.first;
+    adr_Pasien P = L.firstP;
     while (P != nullptr) {
         cout << "Nama Pasien: " << P -> info.namaP << endl;
         cout << "ID Pasien: " << P -> info.ID_Pasien << endl;
         cout << "Keluhan: " << P -> info.keluhan << endl;
         cout << "------------------------" << endl;
-        P = P -> next;
+        P = P -> nextP;
     }
 }
+
