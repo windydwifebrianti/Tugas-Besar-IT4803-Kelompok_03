8a74e976338cb96c1d2ff8ba1dae88f00cf6b8f5
Windy Dwi Febrianti <windydf25@gmail.com>
2025-12-17 15:21:41 +0700

    Menambahkan main.cpp

--- Tugas-Besar-IT4803-Kelompok_03/main.cpp
+++ Tugas-Besar-IT4803-Kelompok_03/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include "main.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int option=-99;
+    while (option != 0) {
+        system("cls");
+        cout << "============ Menu ============ " << endl;
+        cout << "|| 1. Test Case             ||" << endl;
+        cout << "|| 2. Study Case            ||" << endl;
+        cout << "|| 0. exit                  ||" << endl;
+        cout << "============================== " << endl;
+        cout << "Choose your option : ";
+        cin >> option;
+        switch(option) {
+           case 1  :
+              cout << "you choose option 1" << endl;
+              menuAdmin();
+              break;
+           case 2  :
+              cout << "you choose option 2" << endl;
+              menuUser();
+              break;
+
+           default :
+              exit(1);
+        }
+    }
+
+
     return 0;
 }
