154bffb28938736839af6e62bc7f88b9c9564568
ares <arkeiiuc3@gmail.com>
2025-12-18 21:33:25 +0700

    Refactor main.h to include additional headers and externs
    Added includes for Dokter, Pasien, and Relation headers, and declared extern variables for lists.

--- Tugas-Besar-IT4803-Kelompok_03/main.h
+++ Tugas-Besar-IT4803-Kelompok_03/main.h
@@ -1,14 +1,26 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 
+#include "Dokter.h"
+#include "Pasien.h"
+#include "Relation.h"
+
+extern list_Dokter LD;
+extern list_Pasien LP;
+extern list_Relasi LR;
+
+// ===== MENU =====
 void menuAdmin();
 void menuParent();
 void menuChild();
 void menuRelasi();
 
 void menuUser();
-#endif // MAIN_H_INCLUDED
 
+// ===== DUMMY DATA =====
+void initDummyData();
+
+#endif // MAIN_H_INCLUDED
